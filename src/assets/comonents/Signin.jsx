4b2167a0c0d8a69c510995d342ed9afc307e6c76
Galevi1998 <135505755+Galevi1998@users.noreply.github.com>
2025-06-22 16:05:54 +0300

    navbar and signin

---
+++ src/assets/comonents/Signin.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import bgImage from "../../../public/clef.jpg";
+import { FcGoogle } from "react-icons/fc";
+
+export default function Signin() {
+  return (
+    <div className="relative flex items-center justify-center min-h-screen bg-black/20 overflow-hidden px-4">
+      <div
+        className="absolute inset-0 bg-cover bg-center opacity-20"
+        style={{ backgroundImage: `url(${bgImage})` }}
+      />
+
+      <div className="w-full max-w-sm bg-white/80 backdrop-blur-xl px-6 py-8 sm:px-8 rounded-3xl shadow-xl z-10 border border-white/20">
+        <h2 className="text-2xl sm:text-3xl font-bold mb-6 text-center text-gray-800 tracking-tight">
+          Welcome Back
+        </h2>
+
+        <div>
+          <div className="mb-4">
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Email
+            </label>
+            <input
+              type="email"
+              className="w-full p-2 rounded-xl border border-gray-300 focus:ring-2 focus:ring-blue-500 outline-none"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+
+          <div className="mb-6">
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Password
+            </label>
+            <input
+              type="password"
+              className="w-full p-2 rounded-xl border border-gray-300 focus:ring-2 focus:ring-blue-500 outline-none"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+
+          <div className="relative my-4 grid-cols-1 gap-6">
+            <hr className="border-gray-300" />
+            <div className="flex items-center justify-center mt-2">
+              <span className="text-sm text-gray-500">or</span>
+            </div>
+            <div>
+              <button className="w-full flex items-center justify-center gap-2 bg-white border border-gray-300 rounded-lg px-3 py-1.5 mt-2 shadow-sm hover:bg-gray-50 transition text-sm">
+                <FcGoogle size={16} />
+                <span className="text-gray-700 font-normal">
+                  Sign in with Google
+                </span>
+              </button>
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-600 text-white py-2 rounded-xl hover:bg-blue-700 transition"
+          >
+            Sign In
+          </button>
+
+
+          <div className="flex items-center justify-center mt-2">
+              <span className="text-sm text-gray-500"> Not regist</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
